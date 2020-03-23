94f74b57f701ea706535eab4fbe84f5c6e94cfa1
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-23 01:23:52 -0300

    2.13. Validações com Bean Validation

--- cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -11,11 +11,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
-
-import net.bytebuddy.asm.Advice.This;
 
 @Entity
 public class Titulo {
@@ -23,12 +26,19 @@ public class Titulo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+
+	@NotEmpty(message = "Descrição é obrigatória")
+	@Size(max = 60, message = "A descrição não pode conter mais que 60 caracteres")
 	private String descricao;
 
+	@NotNull(message = "Data de vencimento é obrigatória")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 
+	@NotNull(message = "Valor é obrigatório")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01" )
+	@DecimalMax(value = "9999999.99", message = "Valor não pode ser maior que 9.999.999,99" )
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 
@@ -99,7 +109,7 @@ public class Titulo {
 	public void setStatus(StatusTitulo status) {
 		this.status = status;
 	}
-	
+
 	public boolean isPendente() {
 		return StatusTitulo.PENDENTE.equals(this.status);
 	}
