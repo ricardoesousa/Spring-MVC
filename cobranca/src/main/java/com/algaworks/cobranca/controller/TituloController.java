735347779d4dc4d39daaf2e8db8b328b3db04b51
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-23 22:52:02 -0300

    3.2. Plugin para calendário

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -21,7 +22,7 @@ import com.algaworks.cobranca.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	private static final String CADASTRO_VIEW = "CadastroTitulo";
 
 	@Autowired
@@ -39,9 +40,15 @@ public class TituloController {
 		if (errors.hasErrors()) {
 			return CADASTRO_VIEW;
 		}
-		titulos.save(titulo);
-		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
-		return "redirect:/titulos/novo";
+		
+		try {
+			titulos.save(titulo);
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch (DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, "Formato de data inválido");
+		return CADASTRO_VIEW;
+		}
 	}
 
 	@RequestMapping
@@ -53,19 +60,19 @@ public class TituloController {
 	}
 
 	@RequestMapping("{codigo}")
-	public ModelAndView edicao(@PathVariable ("codigo") Titulo titulo) {
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
 		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(titulo);
 		return mv;
 	}
 
-	@RequestMapping(value="{codigo}", method = RequestMethod.POST)
+	@RequestMapping(value = "{codigo}", method = RequestMethod.POST)
 	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
 		titulos.deleteById(codigo);
 		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
 		return "redirect:/titulos";
 	}
-	
+
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
